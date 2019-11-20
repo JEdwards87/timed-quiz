691f7a61dbbf664e55b366b67c1b70fd558836ab
JEdwards87 <57024833+JEdwards87@users.noreply.github.com>
2019-11-19 22:32:10 -0600

    Add files via upload
    initial commit

---
+++ questions.js
@@ -0,0 +1,45 @@
+let questions = [
+    {
+        question : "A function is?",
+        answer1 : "A dinner party",
+        answer2 : "A large group of salamanders",
+        answer3 : "executed when something invokes it",
+        answer4 : "An expression involving one or more variables",
+        correct : "answer3"
+    },{
+        question : "What does HTML stand for?",
+        answer1 : "Haters Take My Lunch",
+        answer2 : "Hot Tamales Make Leaks",
+        answer3 : "Hyper Text Markup Language",
+        answer4 : "Hungry Teachers Make Lasagna",
+        correct : "answer1"
+    },{
+        question : "A function is?",
+        answer1 : "A dinner party",
+        answer2 : "A large group of salamanders",
+        answer3 : "executed when something invokes it",
+        answer4 : "An expression involving one or more variables",
+        correct : "answer3"
+    },{
+        question : "What does HTML stand for?",
+        answer1 : "Haters Take My Lunch",
+        answer2 : "Hot Tamales Make Leaks",
+        answer3 : "Hyper Text Markup Language",
+        answer4 : "Hungry Teachers Make Lasagna",
+        correct : "answer1"
+    },{
+        question : "A function is?",
+        answer1 : "A dinner party",
+        answer2 : "A large group of salamanders",
+        answer3 : "executed when something invokes it",
+        answer4 : "An expression involving one or more variables",
+        correct : "answer3"
+    },{
+        question : "What does HTML stand for?",
+        answer1 : "Haters Take My Lunch",
+        answer2 : "Hot Tamales Make Leaks",
+        answer3 : "Hyper Text Markup Language",
+        answer4 : "Hungry Teachers Make Lasagna",
+        correct : "answer1"
+    }
+];
